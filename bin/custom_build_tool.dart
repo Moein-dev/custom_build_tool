717d7551edeb9d527542b2d21edfbbff3d52c4f3
Moein-dev <flyman812@gamil.com>
2024-05-21 17:43:23 +0330

    update custom build tool for fix some bugs

--- bin/custom_build_tool.dart
+++ bin/custom_build_tool.dart
@@ -3,21 +3,21 @@ import 'package:args/args.dart';
 
 void main(List<String> arguments) {
   final parser = ArgParser()
-    ..addFlag('no_update', negatable: false, defaultsTo: false)
+    ..addFlag('no-version', negatable: false, defaultsTo: false)
     ..addOption('type', defaultsTo: 'release');
 
   final argResults = parser.parse(arguments);
-  final noUpdate = argResults['no_update'] as bool;
+  final noUpdate = argResults['no-version'] as bool;
   final buildType = argResults['type'] as String;
 
   runShellScript(noUpdate: noUpdate, buildType: buildType);
 }
 
 void runShellScript({required bool noUpdate, required String buildType}) async {
-  final scriptPath = 'scripts/increment_version.sh'; // Update with the actual path
+  final scriptPath = 'increment_version.sh'; // Update with the actual path
 
   final arguments = [
-    if (noUpdate) '--no_update',
+    if (noUpdate) '--no-version',
     buildType,
   ];
 
